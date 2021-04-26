13222c6216f71e5ff075804b2bad4e084361fc38
Kenji Tanaka <kenjitanaka80@gmail.com>
2021-04-26 09:30:59 -0700

    Adding support for multiple URLs

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import requests
 import time
 import json
 
-# url to check
-url = "https://www.sportsmans.com/shooting-gear-gun-supplies/modern-sporting-rifles/fn-scar-17s-308-winchester-162in-blackfde-semi-automatic-modern-sporting-rifle-201-rounds/p/1571047"
+# name / url / was_oos
+urls = [("SCAR 17 - FDE", "https://www.sportsmans.com/shooting-gear-gun-supplies/modern-sporting-rifles/fn-scar-17s-308-winchester-162in-blackfde-semi-automatic-modern-sporting-rifle-201-rounds/p/1571047"),
+        ("SCAR 17 - black", "https://www.sportsmans.com/shooting-gear-gun-supplies/modern-sporting-rifles/fn-scar-17s-semi-automatic-rifle/p/1312376")]
 # set the headers like we are a browser,
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 # telegram bot token file
@@ -14,25 +15,27 @@ out_url = f"https://api.telegram.org/bot{token}/sendMessage"
 # id of the chat to send the message to. Hardcoded
 chat_id = "-552787100"
 
-# We'll start in the state assuming that the product was oos
-was_oos = True
+# so we only notify once
+was_oos = [True, True, True]
 
 if __name__=='__main__':
-    # download the homepage
-    payload = {'chat_id': chat_id, 'text': f'Bot is now starting, checking: {url}'}
-    requests.post(out_url, params=payload)
+    for _, url in urls:
+        payload = {'chat_id': chat_id, 'text': f'Bot is now checking: {url}'}
+        requests.post(out_url, params=payload)
 
     while True:
-        r = requests.get(url, headers=headers)
+        for i, (name, url) in enumerate(urls):
+            r = requests.get(url, headers=headers)
 
-        http = r.content.decode()
-        if not "\"btn btn-nostock\"" in http and was_oos:
-            print("change detected, sending message to group.")
-            payload = {'chat_id': chat_id, 'text': f'SCAR 17 now in stock: {url}'}
-            requests.post(out_url, params=payload)
-            was_oos=False
-        elif "\"btn btn-nostock\"" in http and not was_oos:
-            was_oos = True
-        
-        print("Waiting 60 seconds before retrying.")
+            http = r.content.decode()
+
+            if "addToCartButton" in http and was_oos:
+                print("change detected, sending message to group.")
+                payload = {'chat_id': chat_id, 'text': f'{name} now in stock: {url}'}
+                requests.post(out_url, params=payload)
+                was_oos[i]=False
+            elif not "addToCartButton" in http and not was_oos:
+                was_oos[i]=True
+            
+            print("Waiting 60 seconds before retrying.")
         time.sleep(60)
